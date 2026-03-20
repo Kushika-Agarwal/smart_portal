ae20db2ec8a130e4865b030d709c180a1642958a
Kushika-Agarwal <kushikaagarwalg1080@gmail.com>
2026-03-20 13:08:18 +0530

    layout updated

--- smart_card_frontend/src/components/Layout.jsx
+++ smart_card_frontend/src/components/Layout.jsx
@@ -6,25 +6,24 @@ function Layout({ children }) {
 
       {/* GLOBAL NAVBAR */}
    <header className="w-full bg-white shadow-md">
-  <div className="flex items-center justify-between px-10 py-3">
+  <div className="flex flex-wrap items-center justify-between px-4 md:px-10 py-3 gap-2">
     
     {/* Left Logo */}
     <img
       src="/logo_smart_card.png"
       alt="logo"
-      className="h-25"
+      className="h-10 md:h-16 object-contain"
     />
 
     {/* Right Logo */}
     <img
       src="/mod logo.png"
       alt="logo"
-      className="h-25"
+      className="h-10 md:h-16 object-contain"
     />
 
   </div>
 </header>
-
       {/* PAGE CONTENT */}
       <main className="flex-1">
         {children}
@@ -32,7 +31,7 @@ function Layout({ children }) {
 
       {/* GLOBAL FOOTER */}
    {/* GLOBAL FOOTER */}
-<footer className="text-center text-md text-white py-4 bg-gradient-to-r from-slate-700 to-teal-600">
+<footer className="text-center text-sm md:text-md text-white py-4 px-4 bg-gradient-to-r from-slate-700 to-teal-600">
   <p className="opacity-90">
     For any additional inputs, please quote the above query number and send an email on{" "}
     <a
